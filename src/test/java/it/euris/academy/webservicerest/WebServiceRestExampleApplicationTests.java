9f8cccc011b361a20518ad71a979ab8ac11b4457
Roberto Corbatto <roberto.corbatto@euris.it>
2023-09-27 12:25:34 +0200

    First Commit

---
+++ src/test/java/it/euris/academy/webservicerest/WebServiceRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.academy.webservicerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
