8b8c7f9f99507898fc0daee263439c0eb5d9b4c3
Roberto Corbatto <roberto.corbatto@euris.it>
2023-10-02 12:17:23 +0200

    Clean code

--- src/main/java/it/euris/academy/webservicerest/dto/archetype/Dto.java
+++ src/main/java/it/euris/academy/webservicerest/dto/archetype/Dto.java
@@ -1,7 +1,5 @@
 package it.euris.academy.webservicerest.dto.archetype;
 
 public interface Dto {
-
   public Model toModel();
-
 }
--- src/main/java/it/euris/academy/webservicerest/dto/archetype/Model.java
+++ src/main/java/it/euris/academy/webservicerest/dto/archetype/Model.java
@@ -1,7 +1,5 @@
 package it.euris.academy.webservicerest.dto.archetype;
 
 public interface Model {
-
   public Dto toDto();
-
 }
